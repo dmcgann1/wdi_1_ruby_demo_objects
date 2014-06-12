4dc0199e2be4e6e3d1db44a88e6d3aeae0118cd7
dmcgann1 <dmcgann1@gmail.com>
2014-06-12 15:56:19 -0400

    added person bin file

--- bin/person_app.rb
+++ bin/person_app.rb
@@ -1,2 +1,28 @@
 # This will "insert" the contents of the person.rb here
 require_relative '../lib/person.rb'
+
+tom = Person.new('Tom', 'Dyer', '06-02-1957')
+jill = Person.new('Jill', 'Stein', '05-13-1987')
+
+puts "Tom's first name is #{tom.first_name}"
+puts "Jill's last name is #{jill.last_name}"
+
+tom.last_name = 'Jones'
+puts "Tom's last name is #{tom.last_name}"
+
+puts tom.inspect
+
+puts "toms full name is #{tom.full_name}"
+
+puts tom.age
+
+msg = tom.give_insurance? ? "should" : 'should not'
+puts "Tom #{msg} get insured"
+
+tom.current_smoker = true
+msg = tom.give_insurance? ? "should" : 'should not'
+puts "Tom #{msg} get insured"
+
+tom.married = true
+msg = tom.give_insurance? ? "should" : 'should not'
+puts "Tom #{msg} get insured"
